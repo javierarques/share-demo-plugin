41d8fc3890f44ed7df05167ee13f82f15645125c
Javi Arques <javiarchena@gmail.com>
2014-05-26 20:37:57 +0200

    remove console log

--- js/share-demo-plugin.js
+++ js/share-demo-plugin.js
@@ -11,11 +11,11 @@
     };
 
     var buttons = document.body.querySelectorAll('a.share-demo-plugin-button');
+
     if ( buttons.length > 0 ) {
         for ( var i=0; i< buttons.length; i++) {
             buttons[i].addEventListener("click", openNewWindow, false);
         }
     }
 
-    console.log( buttons );
 })( window, document);
